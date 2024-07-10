e14ce2cd1b84af10afff45db2eae785c57b3010a
richeek45 <richeek45@gmail.com>
2024-07-10 11:49:38 +0530

    segrate 0, 1 using 2 pointer approach

--- segregate_zero_one.js
+++ segregate_zero_one.js
@@ -10,6 +10,7 @@ function swap(arr, i, j) {
   arr[j] = temp;
 }
 
+// 1 full iteration O(n)
 function segregate(arr) {
   let index = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -22,6 +23,30 @@ function segregate(arr) {
 }
 
 
+// n/2 iterations O(n)
+function segregate2(arr) {
+  // 2 pointer approach
+  let left = 0, right = arr.length - 1;
+
+  while (left < right) {
+    // shift to right till finding first 1
+    while (arr[left] === 0 && left < right) {
+      left++;
+    }
+
+    // shift to the left till finding first 0
+    while (arr[right] === 1 && left < right) {
+      right--;
+    }
+
+    // swap the 1 on the left with 0 on the right
+    swap(arr, left, right);
+    
+  }
+
+}
+
+
 let arr = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0];
-segregate(arr);
+segregate2(arr);
 console.log(arr);
