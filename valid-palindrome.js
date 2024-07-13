0aab52534a7a0d79e9354cfe4bd2ae0365732796
richeek45 <richeek45@gmail.com>
2024-07-13 11:22:37 +0530

    valid palindrome ignoring alphanumeric characters

--- valid-palindrome.js
+++ valid-palindrome.js
@@ -14,22 +14,31 @@
 // Explanation: s is an empty string "" after removing non-alphanumeric characters.
 // Since an empty string reads the same forward and backward, it is a palindrome.
 
+function isAlphanumeric(i, str) {
+  const chVal = str[i].charCodeAt() - 'a'.charCodeAt(0);
+
+  if (chVal >= 0 && chVal <= 26) {
+      return false;
+  }
+  return true;
+}
+
 function validPalindrome(str) {
   // remove all space and alphanumeric characters 
   str = str.toLowerCase();
-  let res = '';
-
-  // Only keeping lowercase alphabet
-  for (const ch of str) {
-    const chVal = ch.charCodeAt() - 'a'.charCodeAt(0);
-    if (chVal >= 0 && chVal <= 26) {
-      res += ch;
-    }
-  }
-
-  let left = 0, right = res.length - 1;
+  
+  let left = 0, right = str.length - 1;
   while(left < right) {
-    if (res[left] !== res[right]) {
+
+    while(isAlphanumeric(left, str)) {
+      left++;
+    }
+    while(isAlphanumeric(right, str)) {
+      right--;
+    }
+
+    
+    if (str[left] !== str[right]) {
       return false;
     }
     left++; 
