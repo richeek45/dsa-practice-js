e965a54b9ac3e5fe6009340ebb4ca5ce7f9e634d
richeek45 <richeek45@gmail.com>
2024-07-13 12:22:34 +0530

    edge cases

--- three-sum.js
+++ three-sum.js
@@ -8,22 +8,25 @@
 
 function threesum(arr) {
   arr.sort();
-  let i = 0; j = 1, k = arr.length - 1;
+  let i = 0;
   let res = [];
   for (let i = 0; i < arr.length - 2; i++) {
+    // Ignoring duplicates
+    if (i > 0 && arr[i-1] === arr[i]) {
+      continue;
+    }
+
+    let j = i+1, k = arr.length - 1;
     while(j < k) {
       const total = arr[i] + arr[j] + arr[k];
-
-      // Ignoring duplicates
-      if (i > 0 && arr[i-1] === arr[i]) {
-        i++;
-        continue;
-      }
 
       if (total === 0) {
         res.push([arr[i], arr[j], arr[k]]);
         j++;
         k--;
+        while(j < k && arr[j-1] === arr[j]) {
+          j++;
+        }
       }
       
       if (total < 0) {
