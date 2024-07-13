3a4c9d4e72cd9f6a50f837cba77e7264b923c260
richeek45 <richeek45@gmail.com>
2024-07-13 11:53:39 +0530

    sort and remove duplicate then 2 sum

---
+++ three-sum.js
@@ -0,0 +1,45 @@
+// 15. 3Sum
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
+// such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+
+function threesum(arr) {
+  arr.sort();
+  let i = 0; j = 1, k = arr.length - 1;
+  let res = [];
+  for (let i = 0; i < arr.length - 2; i++) {
+    while(j < k) {
+      const total = arr[i] + arr[j] + arr[k];
+
+      // Ignoring duplicates
+      if (i > 0 && arr[i-1] === arr[i]) {
+        i++;
+        continue;
+      }
+
+      if (total === 0) {
+        res.push([arr[i], arr[j], arr[k]]);
+        j++;
+        k--;
+      }
+      
+      if (total < 0) {
+        j++;
+      }
+      if (total > 0) {
+        k--;
+      }
+      
+    }
+    j = i+1;
+    k = arr.length - 1;
+  }
+
+  return res;
+}
+
+const nums = [-1,0,1,2,-1,-4];
+console.log(threesum(nums));
