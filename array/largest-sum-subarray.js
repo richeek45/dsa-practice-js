1650589927df0374cadc0bd0c61b85698b674a6e
richeek45 <richeek45@gmail.com>
2024-08-08 14:49:21 +0530

    largest sum subarray kadane algorithm

---
+++ array/largest-sum-subarray.js
@@ -0,0 +1,25 @@
+// Largest Sum Contiguous Subarray (Kadane’s Algorithm)
+
+const arr = [-2,-3,4,-1,-2,1,5,-3];
+
+function largestSumArray(arr) {
+  let n = arr.length;
+  let left = 0, right = 0;
+  let sum = 0, max = 0;
+  while(right < n) {
+    sum += arr[right++];
+
+  if (sum < 0) {
+    sum = 0;
+    left = 0;
+  }
+
+  if (sum > max) {
+    max = sum;
+  }
+  }
+
+  console.log(max);
+}
+
+largestSumArray(arr);
