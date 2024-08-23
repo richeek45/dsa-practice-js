272190a4962f5793b752ebf1f13e626ba9131119
richeek45 <richeek45@gmail.com>
2024-08-24 00:07:52 +0530

    optimized solution

--- array/maxRotateSum.js
+++ array/maxRotateSum.js
@@ -1,4 +1,10 @@
-const arr = [8, 3, 1, 2];
+const arr1 = [8, 3, 1, 2];
+const arr2 = [3, 2, 1];
+
+// {8, 3, 1, 2} = 8*0 + 3*1 + 1*2 + 2*3 = 11
+// {3, 1, 2, 8} = 3*0 + 1*1 + 2*2 + 8*3 = 29
+// {1, 2, 8, 3} = 1*0 + 2*1 + 8*2 + 3*3 = 27
+// {2, 8, 3, 1} = 2*0 + 8*1 + 3*2 + 1*3 = 17
 
 function maxRotateSum(arr) {
     let res = Number.MIN_VALUE;
@@ -16,4 +22,28 @@ function maxRotateSum(arr) {
     return res;
 }
 
-console.log(maxRotateSum(arr));
+function maxRotateSum1(arr) {
+    let n = arr.length;
+    let res = Number.MIN_VALUE;
+
+    let currSum = 0;
+    for (let i = 0; i < n; i++) {
+        currSum += arr[i];
+    }
+
+    let currVal = 0;
+    for (let i = 0; i < n; i++) {
+        currVal += i * arr[i];
+    }
+
+    for (let i = 1; i < n; i++) {
+        let nextVal = currVal - (currSum - arr[i-1]) + (n-1) * arr[i-1];
+        currVal = nextVal;
+        res = Math.max(res, currVal);
+    }
+    
+    return res;
+}
+
+// console.log(maxRotateSum(arr1));
+console.log(maxRotateSum1(arr1));
