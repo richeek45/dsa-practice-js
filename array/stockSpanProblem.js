43ea9f02920c7e1c4ac95c4dec9ec70d090afa2b
richeek45 <richeek45@gmail.com>
2024-09-10 15:58:02 +0530

    using stack counting stock span

--- array/stockSpanProblem.js
+++ array/stockSpanProblem.js
@@ -1,4 +1,5 @@
-const price = [ 10, 4, 5, 90, 120, 80 ];
+// const price = [ 10, 4, 5, 90, 120, 80 ];
+const price = [ 140, 10, 4, 5, 90, 120, 80 ];
 
 function calculateStockSpan(arr) {
     let n = arr.length;
@@ -20,4 +21,26 @@ function calculateStockSpan(arr) {
     return stock;
 }
 
-console.log(calculateStockSpan(price));
+// O (n)
+function calculateStockSpan1(arr) {
+  let n = arr.length;
+  let stock = new Array(n).fill(0);
+  let stack = [];
+  stack.push(0);
+  stock[0] = 1;
+
+  for (let i = 1; i < n; i++) {
+    let count = 1;
+    while(arr[stack[stack.length-1]] < arr[i]) {
+      let index = stack.pop();
+      count += stock[index];
+    }
+
+    stack.push(i);
+    stock[i] = count;
+  }
+
+  return stock;
+}
+
+console.log(calculateStockSpan1(price));
